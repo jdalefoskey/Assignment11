1f9a03c6f9732bb0e6e85c2c7b2b0e4fdc1a12ad
Dale <developer.dale.foskey@gmail.com>
2021-04-08 21:27:41 -0400

    updated the php file

--- Foskey-A11-sales.php
+++ Foskey-A11-sales.php
@@ -1,59 +1,79 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<!--Author: Johnny Foskey
 		Date:  04/06/21
 		File:	  Foskey-A11-sales.php
 		Purpose: To calculate array average sales
 	
-	sales.php algorithm:
-		Received the button click
-		Open/Read sales-data.txt file
-		While not the end of the file,
-		create array = smith.sales
-		create array = jones.sales
-		calculate array length
-		calculate array average 
-		
-	Display: salesPerson, numberOfSales, totalSalesAmount, salesAverage
+		Foskey-A11-sales.php algorithm:
+			Received the button click
+			Open/Read sales-data.txt file
+			While not the end of the file,
+			create array = smithSales
+			create array = jonesSales
+			Close File
+			calculate sum of array items
+			calculate the average of array items
+			Display: salesPerson, totalSalesMade, totalSalesAmount, salesAverage
 	END
 	-->
 	<title>Sales Report</title>
-	<link rel ="stylesheet" type="text/css" href="sample.css">
+	<link rel="stylesheet" type="text/css" href="sample.css">
 </head>
+
 <body>
 
 	<?php
- $smithSales = [];
- $jonesSales = [];
- // open the file
- $salesData = fopen('sales-data.txt', 'r');
 
- while (!feof($salesData)) {
-     $sales = fgets($salesData);
-     $sales = explode(':', $sales);
-     list($salesPerson, $salesDollars) = $sales;
+	// creating our empty arrays to use
+	$smithSales = [];
+	$jonesSales = [];
 
-     if ($salesPerson == 'Smith') {
-         //here we are pushing Smith sales into an array called smithSales
-         $smithSales[] = floatval($salesDollars);
-     } else {
-         $jonesSales[] = floatval($salesDollars); // here we pust the non- Smith sales into an array called jonesSales
-     }
- }
- //  closing the file after we are done
- fclose($salesData);
- $smithSum = 0;
- for ($i = 0; $i < sizeof($smithSales); $i = $i + 1) {
-     $smithSum = $smithSum + $smithSales[$i];
- }
- $smithAverage = $smithSum / sizeof($smithSales);
- print $smithAverage;
- print '<p> Mr. Jones has ...</p>';
- print '<p> Mr. Smith has ...</p>';
- ?>
-	<!-- Update the link below with the name of your html file -->
+	// open the file
+	$salesData = fopen('sales-data.txt', 'r');
+	// while loop 
+	while (!feof($salesData)) {
+		$sales = fgets($salesData);
+		$sales = explode(':', $sales);
+		list($salesPerson, $salesDollars) = $sales;
+
+		if ($salesPerson == 'Smith') {
+			//here we are pushing Smith sales into an array called smithSales
+			$smithSales[] = floatval($salesDollars);
+		} else {
+			$jonesSales[] = floatval($salesDollars); // here we pust the non Smith sales into an array called jonesSales
+		}
+	}
+	//  closing the file after we are done
+	fclose($salesData);
+
+	// Smith for loop
+	$smithSum = 0;
+	for ($i = 0; $i < sizeof($smithSales); $i = $i + 1) {
+		$smithSum = $smithSum + $smithSales[$i];
+	}
+	$smithCount = sizeof($smithSales);
+	$smithAverage = floatval($smithSum / sizeof($smithSales));
+
+	// Jones for loop
+	$jonesSum = 0;
+	for ($i = 0; $i < sizeof($jonesSales); $i = $i + 1) {
+		$jonesSum = $jonesSum + $jonesSales[$i];
+	}
+	$jonesCount = sizeof($jonesSales);
+	$jonesAverage = floatval($jonesSum / sizeof($jonesSales));
+
+
+
+	//print our results from the forloops
+	print("<p>Smith achieved $smithCount sales with a total value of $$smithSum, and an average sale value of $$smithAverage.</p>");
+	print("<p>Jones achieved $jonesCount sales with a total value of $$jonesSum, and an average sale value of $$jonesAverage.</p>");
+	?>
+	<!-- Link back to html page -->
 	<a href="Foskey-A11-sales.html">Return to form</a>
 
 </body>
+
 </html>
