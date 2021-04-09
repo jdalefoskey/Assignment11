afeaafe2c7131115bf6e963f731562251d5c12b9
Dale Foskey <developer.dale.foskey@gmail.com>
2021-04-09 16:50:13 -0400

    updated html

--- Foskey-A11-sales.php
+++ Foskey-A11-sales.php
@@ -26,55 +26,52 @@
 <body>
 
 	<?php
+ // creating our empty arrays to use
+ $smithSales = [];
+ $jonesSales = [];
 
-	// creating our empty arrays to use
-	$smithSales = [];
-	$jonesSales = [];
+ // open the file
+ $salesData = fopen('sales-data.txt', 'r');
+ // while loop
+ while (!feof($salesData)) {
+     $sales = fgets($salesData);
+     $sales = explode(':', $sales);
+     list($salesPerson, $salesDollars) = $sales;
 
-	// open the file
-	$salesData = fopen('sales-data.txt', 'r');
-	// while loop 
-	while (!feof($salesData)) {
-		$sales = fgets($salesData);
-		$sales = explode(':', $sales);
-		list($salesPerson, $salesDollars) = $sales;
+     if ($salesPerson == 'Smith') {
+         //here we are pushing Smith sales into an array called smithSales
+         $smithSales[] = floatval($salesDollars);
+     } else {
+         $jonesSales[] = floatval($salesDollars); // here we pust the non Smith sales into an array called jonesSales
+     }
+ }
+ //  closing the file after we are done
+ fclose($salesData);
 
-		if ($salesPerson == 'Smith') {
-			//here we are pushing Smith sales into an array called smithSales
-			$smithSales[] = floatval($salesDollars);
-		} else {
-			$jonesSales[] = floatval($salesDollars); // here we pust the non Smith sales into an array called jonesSales
-		}
-	}
-	//  closing the file after we are done
-	fclose($salesData);
+ // Smith for loop
+ $smithSum = 0;
+ for ($i = 0; $i < sizeof($smithSales); $i = $i + 1) {
+     $smithSum = $smithSum + $smithSales[$i];
+ }
+ $smithCount = sizeof($smithSales);
+ $smithAverage = floatval($smithSum / sizeof($smithSales));
 
-	// Smith for loop
-	$smithSum = 0;
-	for ($i = 0; $i < sizeof($smithSales); $i = $i + 1) {
-		$smithSum = $smithSum + $smithSales[$i];
-	}
-	$smithCount = sizeof($smithSales);
-	$smithAverage = floatval($smithSum / sizeof($smithSales));
+ // Jones for loop
+ $jonesSum = 0;
+ for ($i = 0; $i < sizeof($jonesSales); $i = $i + 1) {
+     $jonesSum = $jonesSum + $jonesSales[$i];
+ }
+ $jonesCount = sizeof($jonesSales);
+ $jonesAverage = floatval($jonesSum / sizeof($jonesSales));
 
-	// Jones for loop
-	$jonesSum = 0;
-	for ($i = 0; $i < sizeof($jonesSales); $i = $i + 1) {
-		$jonesSum = $jonesSum + $jonesSales[$i];
-	}
-	$jonesCount = sizeof($jonesSales);
-	$jonesAverage = floatval($jonesSum / sizeof($jonesSales));
-
-
-
-	//print our results from the forloops
-	print("<p>Smith achieved $smithCount sales with a total value of $$smithSum, and an average sale value of $$smithAverage.</p>");
-	print("<p>Jones achieved $jonesCount sales with a total value of $$jonesSum, and an average sale value of $$jonesAverage.</p>");
-	?>
+ //print our results from the forloops
+ print "<p>Smith achieved $smithCount sales with a total value of $$smithSum, and an average sale value of $$smithAverage.</p>";
+ print "<p>Jones achieved $jonesCount sales with a total value of $$jonesSum, and an average sale value of $$jonesAverage.</p>";
+ ?>
 	<!-- Link back to html page -->
 
 	<p><img src="https://media.makeameme.org/created/brace-yourselves-expense.jpg" alt="funny picture"></p>
-	<p><a href="index.html">Return to form</a></p>
+	<p><a href="Foskey-A11-sales.html">Return to form</a></p>
 
 </body>
 
